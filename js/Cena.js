d209e5b3be498ad1415dffdd4889935f14e7ab5d
saviorbp <savio.ribeiro9875@gmail.com>
2021-02-22 21:01:13 -0300

    Adiciona controle de play e pause

--- js/Cena.js
+++ js/Cena.js
@@ -1,38 +1,50 @@
 export default class Cena {
-    /*
+  /*
      É responsável por desenhar elementos na tela em uma animação.
     */
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext("2d");
-        this.sprites = [];
-        this.t0 = 0;
-        this.dt = 0;
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.sprites = [];
+    this.t0 = 0;
+    this.dt = 0;
+    this.idAnim = null;
+  }
+  desenhar() {
+    this.ctx.fillStyle = "grey";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    for (let s = 0; s < this.sprites.length; s++) {
+      const sprite = this.sprites[s];
+      sprite.desenhar(this.ctx);
     }
-    desenhar() {
-        this.ctx.fillStyle = "grey";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        for (let s = 0; s < this.sprites.length; s++) {
-            const sprite = this.sprites[s];
-            sprite.desenhar(this.ctx);
-        }
+  }
+  adicionar(sprite) {
+    this.sprites.push(sprite);
+  }
+  passo(dt) {
+    for (const sprite of this.sprites) {
+      sprite.passo(dt);
     }
-    adicionar(sprite){
-        this.sprites.push(sprite);
-    }
-    passo(dt){
-        for (const sprite of this.sprites) {
-            sprite.passo(dt);
-        }
-    }
+  }
 
-    quadro(t){
-        this.t0 = this.t0 ?? t;
-        this.dt = (t - this.t0) / 1000;
+  quadro(t) {
+    this.t0 = this.t0 ?? t;
+    this.dt = (t - this.t0) / 1000;
 
-        this.passo(this.dt);
-        this.desenhar();
+    this.passo(this.dt);
+    this.desenhar();
 
-        this.t0 = t;
-    }
+    this.iniciar();
+    this.t0 = t;
+  }
+  iniciar() {
+    this.idAnim = requestAnimationFrame(
+        (t) => {this.quadro(t);}
+        );
+  }
+  parar() {
+    cancelAnimationFrame(this.idAnim);
+    this.t0 = null;
+    this.dt = 0;
+  }
 }
--- js/Main.js
+++ js/Main.js
@@ -3,10 +3,24 @@ import Sprite from "./Sprites.js";
 
 const canvas = document.querySelector("canvas");
 const cena1 = new Cena(canvas);
-const pc = new Sprite();
-const en1 = new Sprite({x:140, w:30, color:"red"});
+const pc = new Sprite({ vx: 10 });
+const en1 = new Sprite({ x: 140, w: 30, color: "red" });
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
 
-cena1.quadro(0);
+cena1.iniciar();
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "s":
+      cena1.iniciar();
+      break;
+    case "S":
+      cena1.parar();
+      break;
+
+    default:
+      break;
+  }
+});
