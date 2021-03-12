d873642815f8644fbde3abd781fd5d8a6e48eef2
saviorbp <savio.ribeiro9875@gmail.com>
2021-03-11 22:53:39 -0300

    Adiciona teste de colisão de sprites

--- js/Sprites.js
+++ js/Sprites.js
@@ -1,22 +1,38 @@
 export default class Sprite {
-    /*
-     É responsável por modelar algo que se move na tela.
-    */
-    constructor({x = 100, y = 100, vx = 0, vy = 0, w = 20, h = 20, color = "white"}={}){
-        this.x = x;
-        this.y = y;
-        this.vx = vx;
-        this.vy = vy;
-        this.w = w;
-        this.h = h;
-        this.color = color;
-    }
-    desenhar(ctx){
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
-    passo(dt){
-        this.x = this.x + this.vx*dt;
-        this.y = this.y + this.vy*dt;
-    }
+  /*
+       É responsável por modelar algo que se move na tela.
+      */
+  constructor({
+    x = 100,
+    y = 100,
+    vx = 0,
+    vy = 0,
+    w = 20,
+    h = 20,
+    color = "white",
+  } = {}) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+  }
+  desenhar(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+  passo(dt) {
+    this.x = this.x + this.vx * dt;
+    this.y = this.y + this.vy * dt;
+  }
+  colidiuCom(outro) {
+    return !(
+      this.x > outro.x + outro.w ||
+      this.x + this.w < outro.x ||
+      this.y > outro.y + outro.h ||
+      this.y + this.h < outro.y
+    );
+  }
 }
