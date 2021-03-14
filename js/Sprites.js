14458a9be029d03bbac91a23fb6d73e9cdff0f96
saviorbp <savio.ribeiro9875@gmail.com>
2021-03-14 17:59:21 -0300

    Adiciona Restricoes a esquerda

--- js/Sprites.js
+++ js/Sprites.js
@@ -48,6 +48,10 @@ export default class Sprite {
     );
   }
   aplicaRestricoes(dt) {
+    this.aplicaRestricoesDireita(dt);
+    this.aplicaRestricoesEsquerda(dt);
+  }
+  aplicaRestricoesDireita(dt){
     const SIZE = this.cena.mapa.SIZE;
     if (this.vx > 0) {
       const pmx = this.mx + 1;
@@ -64,6 +68,27 @@ export default class Sprite {
         if (this.colidiuCom(tile)) {
           this.vx = 0;
           this.x = tile.x - tile.w / 2 - this.w / 2 - 1;
+        }
+      }
+    }
+  }
+  aplicaRestricoesEsquerda(dt){
+    if (this.vx < 0) {
+      const SIZE = this.cena.mapa.SIZE;
+      const pmx = this.mx - 1;
+      const pmy = this.my;
+      if (this.cena.mapa.tiles[pmy][pmx] != 0) {
+        const tile = {
+          x: pmx * SIZE + SIZE / 2,
+          y: pmy * SIZE + SIZE / 2,
+          w: SIZE,
+          h: SIZE
+        };
+        this.cena.ctx.strokeStyle = "white";
+        this.cena.ctx.strokeRect(tile.x - SIZE/2, tile.y - SIZE/2, SIZE, SIZE)
+        if (this.colidiuCom(tile)) {
+          this.vx = 0;
+          this.x = tile.x + tile.w / 2 + this.w / 2 + 1;
         }
       }
     }
