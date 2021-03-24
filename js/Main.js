f4b692fc40ad13128e4bf11a8043b3d1c26cae65
saviorbp <savio.ribeiro9875@gmail.com>
2021-03-24 20:51:32 -0300

    Revert "cria gerenciador de entrada de controle"
    This reverts commit 5df8884bec506e8c871d1855f2568a67bb1aedd1.

--- js/Main.js
+++ js/Main.js
@@ -4,9 +4,7 @@ import Sprite from "./Sprites.js";
 import Mapa from "./mapa.js";
 import Mixer from "./Mixer.js";
 import modeloMapa1 from "../maps/mapa1.js";
-import InputManager from "./InputManager.js";
 
-const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
 
@@ -18,12 +16,6 @@ assets.carregaAudio("boom", "assets/boom.wav");
 const canvas = document.querySelector("canvas");
 canvas.width = 20 * 32;
 canvas.height = 20 * 32;
-
-input.configurarTeclado({
-  "ArrowLeft": "MOVE_ESQUERDA",
-  "ArrowRight": "MOVE_DIREITA",
-  });
-
 const cena1 = new Cena(canvas, assets);
 
 const mapa1 = new Mapa(20, 20, 32);
@@ -31,3 +23,20 @@ mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
 
 cena1.iniciar();
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "s":
+      cena1.iniciar();
+      break;
+    case "S":
+      cena1.parar();
+      break;
+    case "c":
+      assets.play("bate");
+      break;
+    case "b":
+      assets.play("boom");
+      break;
+  }
+});
